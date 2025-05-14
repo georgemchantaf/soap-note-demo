dc6d14d78c66ea10375a7396656b85efda5cb159
georgemchantaf <georgesmchantaf@hotmail.com>
2025-05-14 23:30:40 +0300

    Add files via upload

--- soap.py
+++ soap.py
@@ -21,7 +21,8 @@ def transcribe_openai(audio_path):
     with open(audio_path, "rb") as f:
         transcript = client.audio.transcriptions.create(
             model="whisper-1",
-            file=f
+            file=f,
+            language="en"
         )
     return transcript.text
 
